aede0024d386f8a8520be7a22f37ca7c85f5c87a
anne-soline.guilbert-ly@dataiku.com <anne-soline.guilbert-ly@dataiku.com>
2025-03-14 13:41:55 +0000

    Created python recipe: EDFSENTIMENTANALYSIS.recipe_from_notebook_Import_data_from_internet

---
+++ recipes/recipe_from_notebook_Import_data_from_internet.py
@@ -0,0 +1,94 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE_ASSISTANT_MAGIC_CELL
+# %load_ext ai_code_assistant
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+import os
+import tempfile
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+project = dataiku.api_client().get_default_project()
+client = dataiku.api_client()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Retrieve Kaggle username and api key
+auth_info = client.get_auth_info(with_secrets=True)
+secret_value = None
+for secret in auth_info["secrets"]:
+    if secret["key"] == "KAGGLE_API_KEY":
+        os.environ["KAGGLE_KEY"] = secret["value"]
+        
+    elif secret["key"] == "KAGGLE_USERNAME":
+        os.environ["KAGGLE_USERNAME"] = secret["value"]
+        
+from kaggle import api # import the already authenticated API client
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Retrieve the folder id where the dataset will be stored
+folder_id = next((folder["id"] for folder in project.list_managed_folders() if folder["name"]=="data"), None)
+if folder_id is None:
+    print("Folder 'data' not found!")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import chardet
+folder = dataiku.Folder(folder_id)
+dataset_slug = "kazanova/sentiment140"
+with tempfile.TemporaryDirectory() as tmpdirname:
+    
+    api.dataset_download_files(dataset_slug, path=tmpdirname, unzip=True)
+
+    for file in os.listdir(tmpdirname):
+        local_file = os.path.join(tmpdirname, file)
+        folder.upload_file(file, local_file)
+        annotated_tweets_df = pd.read_csv(local_file, encoding="latin-1")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Write recipe outputs
+annotated_tweets = dataiku.Dataset("annotated_tweets")
+annotated_tweets.write_with_schema(annotated_tweets_df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+import dataiku
+from dataiku import pandasutils as pdu
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Example: load a DSS dataset as a Pandas dataframe
+tweets = dataiku.Dataset("tweets")
+df = tweets.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.info()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(f"Values taken by the column flag: {df['flag'].unique()[0]}")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+print(f"There are {df['user'].nunique()} different users.")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# ## Tweet length analysis
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['tweet_length_chars'] = df['text'].str.len()
+df['tweet_length_words'] = df['text'].str.split().apply(len)
+df.head()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+plt.figure(figsize=(10,5))
+plt.hist(df['tweet_length_chars'], bins=20, color='blue', alpha=0.7)
+plt.xlabel("Tweet length (in characters)")
+plt.ylabel("Number of tweets")
+plt.title("Distribution of tweets length")
+plt.show()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Recipe outputs
+imported_tweets = dataiku.Dataset("imported_tweets")
+imported_tweets.write_with_schema(pandas_dataframe)
