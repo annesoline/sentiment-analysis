39e26ed133b32be2a9a9d41be27d4340085f34e4
anne-soline.guilbert-ly@dataiku.com <anne-soline.guilbert-ly@dataiku.com>
2025-03-27 07:53:19 +0000

    Recipe EDFSENTIMENTANALYSIS.recipe_from_notebook_8_prepare_and_filter_tweets saved through notebook 8_prepare_and_filter_tweets

--- recipes/recipe_from_notebook_8_prepare_and_filter_tweets.py
+++ recipes/recipe_from_notebook_8_prepare_and_filter_tweets.py
@@ -21,7 +21,20 @@ cleaned_tweets_encryption_df = cleaned_tweets_encryption.get_dataframe()
 cleaned_tweets_encryption_df = cleaned_tweets_encryption_df[cleaned_tweets_encryption_df['is_duplicated'] == 0]
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
-# ## 8.2. Stemming
+# ## 8.2. Filter to balance the data on label column
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+cleaned_tweets_encryption_df['label'].value_counts()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+min_count = cleaned_tweets_encryption_df['label'].value_counts().min()
+balanced_df = cleaned_tweets_encryption_df.groupby('label', group_keys=False).apply(lambda x: x.sample(min(len(x), min_count))).reset_index(drop=True)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+balanced_df['label'].value_counts()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
+# ## 8.3. Stemming
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Apply stemming to the 'text' column
@@ -34,9 +47,9 @@ def apply_stemming(text):
     stemmed_words = [stemmer.stem(word) for word in words]
     return ' '.join(stemmed_words)
 
-cleaned_tweets_encryption_df['text'] = cleaned_tweets_encryption_df['text'].apply(apply_stemming)
+balanced_df['text'] = balanced_df['text'].apply(apply_stemming)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe outputs
 prepared_tweets_encryption = dataiku.Dataset("prepared_tweets_encryption")
-prepared_tweets_encryption.write_with_schema(cleaned_tweets_encryption_df)
+prepared_tweets_encryption.write_with_schema(balanced_df)
