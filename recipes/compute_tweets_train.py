e4b7775b5c59cfb7acd8214198f78876272600f8
anne-soline.guilbert-ly@dataiku.com <anne-soline.guilbert-ly@dataiku.com>
2025-03-27 14:40:31 +0000

    Saved recipe: EDFSENTIMENTANALYSIS.compute_tweets_train

--- recipes/compute_tweets_train.py
+++ recipes/compute_tweets_train.py
@@ -1,25 +1,32 @@
 # -*- coding: utf-8 -*-
-import dataiku
+
 import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+
 
 # Read recipe inputs
 prepared_tweets = dataiku.Dataset("prepared_tweets")
 prepared_tweets_df = prepared_tweets.get_dataframe()
 
+# Split the data into 80% training and 20% testing
+train_df = prepared_tweets_df.sample(frac=0.8, random_state=42)
+eval_df = prepared_tweets_df.drop(train_df.index)
+
+
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: MARKDOWN
 # Filter to balance the data on label column
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+train_df['label'].value_counts()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-prepared_tweets_df['label'].value_counts()
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-min_count = prepared_tweets_df['label'].value_counts().min()
-balanced_df = prepared_tweets_df.groupby('label', group_keys=False).apply(lambda x: x.sample(min(len(x), min_count))).reset_index(drop=True)
+min_count = train_df['label'].value_counts().min()
+balanced_df = train_df.groupby('label', group_keys=False).apply(lambda x: x.sample(min(len(x), min_count))).reset_index(drop=True)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 prepared_tweets_df['label'].value_counts()
 
 # Write recipe outputs
 tweets_train = dataiku.Dataset("tweets_train")
-tweets_train.write_with_schema(prepared_tweets_df)
+tweets_train.write_with_schema(balanced_df)
+
+tweets_eval = dataiku.Dataset("tweets_test")
+tweets_eval.write_with_schema(eval_df)
